7fa8aa80b064540911657264219818c4b4cb532b
anmre21 <anmre21@gmail.com>
2021-12-09 18:08:18 -0500

    fix image processing?

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from colorizers import eccv16
+from colorizers import *
 import time
 import glob as glob
 from PIL import Image
@@ -17,45 +17,6 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 from torchsummary import summary
 
-def load_img(img_path):
-	out_np = np.asarray(Image.open(img_path))
-	if(out_np.ndim==2):
-		out_np = np.tile(out_np[:,:,None],3)
-	return out_np
-
-def resize_img(img, HW=(256,256), resample=3):
-	return np.asarray(Image.fromarray(img).resize((HW[1],HW[0]), resample=resample))
-
-def preprocess_img(img_rgb_orig, HW=(256,256), resample=3):
-	# return original size L and resized L as torch Tensors
-	img_rgb_rs = resize_img(img_rgb_orig, HW=HW, resample=resample)
-	
-	img_lab_orig = color.rgb2lab(img_rgb_orig)
-	img_lab_rs = color.rgb2lab(img_rgb_rs)
-
-	img_l_orig = img_lab_orig[:,:,0]
-	img_l_rs = img_lab_rs[:,:,0]
-
-	tens_orig_l = torch.Tensor(img_l_orig)[None,None,:,:]
-	tens_rs_l = torch.Tensor(img_l_rs)[None,None,:,:]
-
-	return (tens_orig_l, tens_rs_l)
-
-def postprocess_tens(tens_orig_l, out_ab, mode='bilinear'):
-	# tens_orig_l 	1 x 1 x H_orig x W_orig
-	# out_ab 		1 x 2 x H x W
-
-	HW_orig = tens_orig_l.shape[2:]
-	HW = out_ab.shape[2:]
-
-	# call resize function if needed
-	if(HW_orig[0]!=HW[0] or HW_orig[1]!=HW[1]):
-		out_ab_orig = F.interpolate(out_ab, size=HW_orig, mode='bilinear')
-	else:
-		out_ab_orig = out_ab
-
-	out_lab_orig = torch.cat((tens_orig_l, out_ab_orig), dim=1)
-	return color.lab2rgb(out_lab_orig.data.cpu().numpy()[0,...].transpose((1,2,0)))
 
 """## Step 1: Build dataloaders for train and test
 
@@ -77,8 +38,7 @@ class Edges2Image(Dataset):
     return len(self.files)
 
   def __getitem__(self, idx):
-    img = Image.open(self.files[idx])
-    img = np.asarray(img)
+    img = load_img(self.files[idx])
     (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
     tens_l_rs = tens_l_rs.cuda()
     return tens_l_rs
@@ -115,14 +75,14 @@ print(fixed_y_.shape)
 fig, axes = plt.subplots(2, 2)
 axes = np.reshape(axes, (4, ))
 for i in range(4):
-  example = train_loader.__iter__().__next__()[i].numpy().transpose((1, 2, 0))
+  example = train_loader.__iter__().__next__()[i].cpu().numpy().transpose((1, 2, 0))
   mean = np.array([0.5, 0.5, 0.5])
   std = np.array([0.5, 0.5, 0.5])
   example = std * example + mean
   axes[i].imshow(example)
   axes[i].axis('off')
 
-plt.show()
+plt.savefig(f'./train_pics/example.png')
 
 
 def train(model, num_epochs = 20):
@@ -199,7 +159,7 @@ def show_result(G, x_, y_, num_epoch):
     ax[i, 0].cla()
     ax[i, 0].imshow(process_image(x_[i]))
     ax[i, 1].cla()
-    ax[i, 1].imshow(process_image(predict_images[i]))
+    ax[i, 1].imshow(process_image(postprocess_tens(y_[i], predict_images[i])))
     ax[i, 2].cla()
     ax[i, 2].imshow(process_image(y_[i]))
   
@@ -213,7 +173,7 @@ def show_result(G, x_, y_, num_epoch):
   label_truth = 'Ground truth'
   fig.text(0.81, 1, label_truth, ha='center')
 
-  print("saving figure")
+  print(f"saving figure for epoch {num_epoch}")
   plt.savefig(f'./train_pics/epoch_{num_epoch}.png')
 
 # Helper function for showing result.
